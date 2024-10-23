7f97791b177d4348ec9ab3547ac8c05960405a04
chandan pal <chandanpal18222gmail.com>
2024-10-23 23:08:54 +0530

    get and post api

--- src/app.js
+++ src/app.js
@@ -2,24 +2,48 @@ const express = require("express");
 
 const connectDB = require("./config/database");
 
-const User = require('./models/user')
+const User = require("./models/user");
 
 const app = express();
 
 app.use(express.json()); // express middleware which takes json object and convert to js object
-                        // then pass js object to req then attaches to .body
+// then pass js object to req then attaches to .body
 
-app.post("/signup", async (req,res)=>{
-    
-
-    const user =new User(req.body)  //creating new instance of the User
-    try{
-        await user.save();
-        res.send("user added successfully");
-    } catch (err){
-        res.status(500).send("error while saving user");
+//get user by emailid
+app.get("/user", async (req, res) => {
+  const Useremail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: Useremail });
+    if(users.length === 0){
+      res.send("oops!! user not found")
+    }else{
+      res.send(users);
     }
    
+  } catch (err) {
+    res.send("error!!!!");
+  }
+});
+
+//post api to signup user
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body); //creating new instance of the User
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(500).send("error while saving user");
+  }
+});
+
+//feed api to get all user
+app.get("/feed", async (rq,res)=>{
+  try{
+    const users= await User.find({});
+    res.send(users)
+  } catch(err){
+    res.status(404).send("user not found")
+  }
 })
 
 connectDB()
